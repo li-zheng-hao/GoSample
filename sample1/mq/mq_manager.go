9d11f2bbd9e6858a4a6b576b61e8502af60c05e6
LiZhengHao <1263212577@qq.com>
2022-11-28 17:50:07 +0800

    update

--- sample1/mq/mq_manager.go
+++ sample1/mq/mq_manager.go
@@ -11,12 +11,34 @@ type MQManager struct {
 	Channel    *amqp.Channel
 }
 
+func (mq *MQManager) Publish(exchange, key string, msg []byte) error {
+	err := mq.Channel.Publish(exchange, key, false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        msg,
+	})
+	return err
+}
+func (mq *MQManager) Consume(key string) {
+	msgs, err := mq.Channel.Consume(key, "", true, false, false, false, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for msg := range msgs {
+		fmt.Println(string(msg.Body))
+	}
+}
+func (mq *MQManager) Close() {
+	mq.Channel.Close()
+	mq.Connection.Close()
+}
+
 var MQManagerInstance *MQManager
 
 func init() {
 	fmt.Println("init mq")
 	MQManagerInstance = &MQManager{}
-	conn, err := amqp.Dial("amqp://")
+	conn, err := amqp.Dial("amqp://admin::5672//cloud")
 	if err != nil {
 		fmt.Println(err)
 		return
